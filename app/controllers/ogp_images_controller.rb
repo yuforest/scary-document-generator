8a1149bbee69beae7ea0df04eba821b5b853ecdb
大森優太 <>
2020-08-27 02:09:11 +0900

    fix little

--- app/controllers/ogp_images_controller.rb
+++ app/controllers/ogp_images_controller.rb
@@ -3,7 +3,7 @@ class OgpImagesController < ApplicationController
     text = ogp_params[:text]
     image = OgpCreator.build(text).tempfile.open.read
     if ogp_params[:download].present?
-      send_data image, type: 'image/png', disposition: 'attachment', filename: "youzyo_document.png"
+      send_data image, type: 'image/png', disposition: 'attachment', filename: "document.png"
     else
       send_data image, :type => 'image/png', :disposition => 'inline'
     end
